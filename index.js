35dfe59ecfedc74a841b55aea1adaa50b943326d
Klemen Slatnar <klemen.slatnar@gmail.com>
2017-04-02 01:28:42 +0200

    Using express static gzip and set view engine to ejs

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const app = express();
 const argv = require('yargs').argv;
+const expressStaticGzip = require("express-static-gzip");
 
 const port = argv.p || argv.port || 3000;
 
+app.set('view engine', 'ejs');
+app.use('/assets', expressStaticGzip("public"));
 app.listen(port, () => {
     console.log(`Server started on port ${port}`);
 }).on('error', (err) => {
