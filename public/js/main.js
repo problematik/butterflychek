2e43a6164691aec20fbcfddb5953f80b9f93539d
Klemen Slatnar <klemen.slatnar@gmail.com>
2017-04-03 01:37:53 +0200

    Extracted picker logic into own class so that it can be reused

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,52 @@
+class Picker {
+    constructor(el, options = {
+        initialValue: 3,
+        valueSetterElement: null
+    }) {
+        this.VALUE_CHANGED = 'VALUE_CHANGED';
+
+        // null is valid value
+        options.initialValue = options.hasOwnProperty('initialValue') ? options.initialValue : 3;
+        options.valueSetterElement = options.valueSetterElement || null;
+
+        this.el = $(el);
+
+        if(options.valueSetterElement === null) {
+            options.valueSetterElement = el;
+        }
+
+        this.setter = $(options.valueSetterElement);
+
+        this.value = options.initialValue;
+
+        this.el.find('.item').on('click', this.setValue.bind(this));
+
+        this.setterElementAddValueClass();
+    }
+
+    setValue(e) {
+        this.setterElementRemoveValueClass();
+        this.value = $(e.currentTarget).data('value');
+        this.setterElementAddValueClass();
+
+        this.el.emit(this.VALUE_CHANGED);
+    }
+
+    on(event, callback) {
+        this.el.on(event, callback);
+    }
+
+    setterElementRemoveValueClass() {
+        this.setter.removeClass(`is-${this.value}`);
+    }
+
+    setterElementAddValueClass() {
+        if(this.value !== null) {
+            this.setter.addClass(`is-${this.value}`);
+        }
+    }
+}
+
 class Overall {
     constructor() {
         this.el = $('.overall .scores');
@@ -15,14 +64,15 @@ class Overall {
         this.el.find('.state span').on('click', this.selecting.bind(this));
         this.el.find('.state svg').on('click', this.selecting.bind(this));
 
-        this.el.find('.items span').on('click', this.selectScore.bind(this));
-        this.setParentScore();
-    }
-    selectScore(e) {
-        this.clearParentScore();
-        this.score = $(e.currentTarget).data('score');
-        this.setParentScore();
-        this.selected();
+        this.picker = new Picker($('.overall .items-wrapper'), {
+            valueSetterElement: this.el,
+            initialValue: this.score
+        });
+
+        this.picker.on(this.picker.VALUE_CHANGED, () => {
+            this.score = this.picker.value;
+            this.selected();
+        });
     }
     selected() {
         this.toggleSelectionClasses();
@@ -33,9 +83,8 @@ class Overall {
     toggleSelectionClasses() {
         this.el.toggleClass('selected selecting');
     }
+}
 
-    clearParentScore() {
-        this.el.removeClass(`is-${this.score}`);
     }
     setParentScore() {
         this.el.addClass(`is-${this.score}`);
