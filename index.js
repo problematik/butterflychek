5384e48050c0642390e80f4d7ae888650d20b547
Klemen Slatnar <klemen.slatnar@gmail.com>
2017-04-03 02:28:31 +0200

    Added thank you page

--- index.js
+++ index.js
@@ -2,18 +2,25 @@ const express = require('express');
 const app = express();
 const argv = require('yargs').argv;
 const expressStaticGzip = require("express-static-gzip");
+const bodyParser = require('body-parser');
 
 const questions = require('./questions').provide();
 
 const port = argv.p || argv.port || 3000;
 
 app.set('view engine', 'ejs');
+
 app.use('/assets', expressStaticGzip("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
     res.render('index', { view: 'questions', data: questions });
 });
+app.post('/', (req, res) => {
+   console.log(req.body);
 
+   res.render('index', { view: 'thanks', data: null});
+});
 app.listen(port, () => {
     console.log(`Server started on port ${port}`);
 }).on('error', (err) => {
@@ -21,6 +28,7 @@ app.listen(port, () => {
     if(err.code === 'EADDRINUSE') {
         return console.error(`Port ${port} already in use. Use -p when starting to provide another port`);
     }
+
 
     console.error(err);
 });
